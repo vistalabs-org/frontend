53baeb481ecc6af91a6a6565b588470b3c8bb3c1
Jared Tokuz <jtokuz@gmail.com>
2025-03-17 19:39:04 -0500

    tailwind and testing config

--- config.ts
+++ config.ts
@@ -13,25 +13,44 @@ dotenv.config();
 // if (!WALLET_CONN_PROJ_ID) throw "env var ALCHEMY_API_KEY missing"
 
 const uiConfig: AlchemyAccountsUIConfig = {
-illustrationStyle: "outline",
-auth: {
-    sections: [
-    [{ type: "email" }],
-    [
-        { type: "passkey" },
-        { type: "social", authProviderId: "google", mode: "popup" },
-        { type: "social", authProviderId: "facebook", mode: "popup" },
-    ],
-    // [
-    //     {
-    //     type: "external_wallets",
-    //     walletConnect: { projectId: WALLET_CONN_PROJ_ID },
-    //     },
-    // ],
-    ],
-    addPasskeyOnSignup: false,
-},
-};
+    illustrationStyle: "outline",
+    auth: {
+      sections: [
+        [{ type: "email", emailMode: "otp" }],
+        [
+          { type: "passkey" },
+          { type: "social", authProviderId: "google", mode: "popup" },
+          { type: "social", authProviderId: "facebook", mode: "popup" },
+        //   {
+        //     type: "social",
+        //     authProviderId: "auth0",
+        //     mode: "popup",
+        //     auth0Connection: "discord",
+        //     displayName: "Discord",
+        //     logoUrl: "/images/discord.svg",
+        //     scope: "openid profile",
+        //   },
+        //   {
+        //     type: "social",
+        //     authProviderId: "auth0",
+        //     mode: "popup",
+        //     auth0Connection: "twitter",
+        //     displayName: "Twitter",
+        //     logoUrl: "/images/twitter.svg",
+        //     // logoUrlDark: "/images/twitter.svg",
+        //     scope: "openid profile",
+        //   },
+        ],
+        // [
+        //   {
+        //     type: "external_wallets",
+        //     walletConnect: { projectId: "your-project-id" },
+        //   },
+        // ],
+      ],
+      addPasskeyOnSignup: false,
+    },
+  };
   
 const ALCHEMY_API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY;
 if (!ALCHEMY_API_KEY) throw "env var ALCHEMY_API_KEY missing"
